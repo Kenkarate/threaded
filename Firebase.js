bdaba7782f7c66b4283c08957b12203af5e03a72
kenkarate <kenkarateindia@gmail.com>
2025-03-03 19:27:49 +0530

    firebase added

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4fYLJdgy-8U8nJ9762BBBIhOtz6HecpQ",
+  authDomain: "threaded-3859b.firebaseapp.com",
+  projectId: "threaded-3859b",
+  storageBucket: "threaded-3859b.firebasestorage.app",
+  messagingSenderId: "671951553200",
+  appId: "1:671951553200:web:b5e54c8f01c73fac597175"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
